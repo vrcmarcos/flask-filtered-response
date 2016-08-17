58d88e72e8e83c8571b74f3aacd8bfedbc40a68a
Marcos Cardoso <vrcmarcos@gmail.com>
2016-08-17 11:31:39 -0300

    Fixed description and url

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='Flask-Filtered-Response',
-    version='1.0.0',
-    url='https://github.com/vrcmarcos/flask-paginated-response',
+    version='1.0.1',
+    url='https://github.com/vrcmarcos/flask-filtered-response',
     license='MIT',
     author='Marcos Cardoso',
     author_email='vrcmarcos@gmail.com',
-    description='Response maker for Flask with RFC Standards such as Link Headers',
+    description='Add filter capability to JSON responses',
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
